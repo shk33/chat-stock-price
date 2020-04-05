f53ea52b8e0ae38ffdb36fc2ac9ba08d0ac898e8
shk33 <m.coronel.seg@gmail.com>
2020-04-05 14:28:49 -0500

    Add router

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 const PORT = process.env.PORT || 5000;
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
