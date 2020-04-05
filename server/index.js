a4bf16b773b805bfcbd87d0a1e33eb2d5d307524
shk33 <m.coronel.seg@gmail.com>
2020-04-05 18:20:44 -0500

    Add basic config for socketio

--- server/index.js
+++ server/index.js
@@ -9,6 +9,14 @@ const io = socketio(server);
 
 const PORT = process.env.PORT || 5000;
 
+io.on('connection', (socket) => {
+    console.log('There is a new connection');
+
+   socket.on('disconnect', () => {
+        console.log('User had left!!');
+   }) 
+});
+
 app.use(router);
 
 server.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
